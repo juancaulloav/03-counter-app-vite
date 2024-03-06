05ee029d8ed7e67bb8e8641966c6c5e971dfd034
Juan Carlos Ulloa Velasquez <juancarlosulloav@gmail.com>
2024-03-06 17:55:12 -0300

    commit inicial

---
+++ src/FirstApp.jsx
@@ -0,0 +1,10 @@
+
+
+export const FirstApp = () => {
+  return (
+    <>
+    <h1>{  }</h1>
+    <p>soy un subtitulo</p>
+    </>
+  )
+}
---
+++ src/HelloWorldApp.jsx
@@ -0,0 +1,7 @@
+
+
+export const HelloWorldApp = () => {
+  return (
+    <h1>HelloWorldApp</h1>
+  )
+}
