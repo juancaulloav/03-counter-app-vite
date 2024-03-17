de4737ffcf6b9e13f491493be92237cec519772f
Juan Carlos Ulloa Velasquez <juancarlosulloav@gmail.com>
2024-03-17 16:53:50 -0300

    final counter y se agregan archivos para estudio de  testing

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,4 +1,4 @@
-
+import PropTypes from 'prop-types';
 const newMessage = {
   nombre: 'juanca',
   apellido: 'ulloa'
@@ -6,12 +6,26 @@ const newMessage = {
 const getResult =  () =>{
     return 'mapache';
 }
-export const FirstApp = () => {
+export const FirstApp = ({ title , subTitle , name }) => {
+
 
   return (
     <>
     <h1>{`Hola  ${getResult()} ${newMessage.apellido} `}</h1>
-    <p>soy un subtitulo</p>
+    <h2>{ title }</h2>
+    <p>{ subTitle }</p>
+    <p>{ name }</p>
     </>
   )
+}
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string
+}
+
+FirstApp.defaultProps = {
+    name: 'Juanca Ulloa',
+    subTitle: ' no hay subtitulo ',
+    title: ' no hay titulo ',
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
-
-
+import { CounterApp } from './CounterApp';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp />
-        <FirstApp />
+        {/* <HelloWorldApp />
+        <FirstApp /> */}
+        <CounterApp value={123}/> 
     </React.StrictMode>
 )
